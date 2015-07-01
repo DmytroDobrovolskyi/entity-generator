c337079f89686c5832517229327633930d30cf38
Oles Onyshchak <oles1719@gmail.com>
2015-07-01 10:06:11 +0300

    add hash code and equals to field

--- src/main/java/com/softserve/entity/generator/entity/Field.java
+++ src/main/java/com/softserve/entity/generator/entity/Field.java
@@ -1,5 +1,8 @@
 package com.softserve.entity.generator.entity;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 import javax.persistence.*;
 
 @javax.persistence.Entity
@@ -40,4 +43,23 @@ public class Field {
         this.type = type;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Field field = (Field) o;
+
+        return new EqualsBuilder()
+                .append(name, field.name)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(name)
+                .toHashCode();
+    }
 }
