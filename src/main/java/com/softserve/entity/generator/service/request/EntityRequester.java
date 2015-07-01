63fb9d5f1d534d8ebeb4986fba8ee6e73637199d
Oles Onyshchak <oles1719@gmail.com>
2015-07-01 14:09:35 +0300

    modify entity trigger

--- src/main/java/com/softserve/entity/generator/service/request/EntityRequester.java
+++ src/main/java/com/softserve/entity/generator/service/request/EntityRequester.java
@@ -1,5 +1,18 @@
 package com.softserve.entity.generator.service.request;
 
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
 public class EntityRequester
 {
+    private String accessToken;
+    private static final String BASE_URL = "https://emea.salesforce.com/services/data/";
+
+    public void getfullEntityInfo()
+    {
+
+        CloseableHttpClient httpPost = HttpClientBuilder.create().build();
+     /*   HttpGet httpGet = new HttpGet(BASE_URL + API_VERSION + "sobjects/Line_Item__c");*/
+}
 }
