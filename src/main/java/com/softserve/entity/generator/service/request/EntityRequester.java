30dd3a8dcd794c9dff8b7fafbcbd4bd4c12da606
Oles Onyshchak <oles1719@gmail.com>
2015-07-01 15:11:28 +0300

    add EntityRequester class

--- src/main/java/com/softserve/entity/generator/service/request/EntityRequester.java
+++ src/main/java/com/softserve/entity/generator/service/request/EntityRequester.java
@@ -1,20 +1,45 @@
 package com.softserve.entity.generator.service.request;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicHeader;
+import org.apache.http.util.EntityUtils;
 
-public class EntityRequester {
+import java.io.IOException;
+
+public class EntityRequester
+{
     private String accessToken;
     private static final String BASE_URL = "https://emea.salesforce.com/services/data/";
     private static final String API_VERSION = "v33.0/";
 
-    public void getfullEntityInfo() {
+    public void getFullEntityInfo()
+    {
         String accessToken = new Authentication().login();
-        CloseableHttpClient httpPost = HttpClientBuilder.create().build();
+
+        HttpClient httpClient = HttpClientBuilder.create().build();
+
         HttpGet httpGet = new HttpGet(BASE_URL + API_VERSION + "sobjects/Entity__c");
         httpGet.addHeader(new BasicHeader("Authorization", "OAuth " + accessToken));
 
+
+        try
+        {
+            HttpResponse response = httpClient.execute(httpGet);
+            System.out.println(EntityUtils.toString(response.getEntity()));
+        }
+        catch (ClientProtocolException ex)
+        {
+
+            throw new AssertionError(ex);
+        }
+        catch (IOException ex)
+        {
+
+            throw new AssertionError(ex);
+        }
     }
 }
