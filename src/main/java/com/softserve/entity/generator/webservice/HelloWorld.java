321474bf5f316294f3ed8811ca9381786c93bc98
DmytroDobrovolskyi <gwini777@Gmail.com>
2015-07-27 10:29:26 +0300

    Add JAX-WS

---
+++ src/main/java/com/softserve/entity/generator/webservice/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.softserve.entity.generator.webservice;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
+@WebService
+@SOAPBinding(style = SOAPBinding.Style.RPC)
+public interface HelloWorld
+{
+    @WebMethod
+    String getHelloWorldAsString();
+}
