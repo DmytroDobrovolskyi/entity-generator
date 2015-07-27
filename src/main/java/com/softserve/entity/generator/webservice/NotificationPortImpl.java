32e0e674c84ade873dae0694a2ea711dbf277b25
DmytroDobrovolskyi <gwini777@gmail.com>
2015-07-27 10:28:54 +0000

    Fix web services

--- src/main/java/com/softserve/entity/generator/webservice/NotificationPortImpl.java
+++ src/main/java/com/softserve/entity/generator/webservice/NotificationPortImpl.java
@@ -1,36 +1,16 @@
 package com.softserve.entity.generator.webservice;
 
-import javax.jws.WebMethod;
-import javax.jws.WebParam;
-import javax.jws.WebResult;
 import javax.jws.WebService;
-import javax.xml.ws.RequestWrapper;
-import javax.xml.ws.ResponseWrapper;
 import java.util.List;
 
 @WebService(name = "NotificationPortImpl", targetNamespace = "http://soap.sforce.com/2005/09/outbound")
 public class NotificationPortImpl implements NotificationPort
 {
     @Override
-    @WebMethod
-    @WebResult(name = "Ack", targetNamespace = "http://soap.sforce.com/2005/09/outbound")
-    @RequestWrapper(localName = "notifications", targetNamespace = "http://soap.sforce.com/2005/09/outbound", className = "com.softserve.entity.generator.webservice.Notifications")
-    @ResponseWrapper(localName = "notificationsResponse", targetNamespace = "http://soap.sforce.com/2005/09/outbound", className = "com.softserve.entity.generator.webservice.NotificationsResponse")
-    public boolean notifications(
-            @WebParam(name = "OrganizationId", targetNamespace = "http://soap.sforce.com/2005/09/outbound")
-            String organizationId,
-            @WebParam(name = "ActionId", targetNamespace = "http://soap.sforce.com/2005/09/outbound")
-            String actionId,
-            @WebParam(name = "SessionId", targetNamespace = "http://soap.sforce.com/2005/09/outbound")
-            String sessionId,
-            @WebParam(name = "EnterpriseUrl", targetNamespace = "http://soap.sforce.com/2005/09/outbound")
-            String enterpriseUrl,
-            @WebParam(name = "PartnerUrl", targetNamespace = "http://soap.sforce.com/2005/09/outbound")
-            String partnerUrl,
-            @WebParam(name = "Notification", targetNamespace = "http://soap.sforce.com/2005/09/outbound")
-            List<EntityCNotification> notification)
+    public boolean notifications(String organizationId, String actionId, String sessionId, String enterpriseUrl, String partnerUrl, List<EntityCNotification> notification)
     {
-        System.out.println("Hello");
+        System.out.println(notification.get(0).getSObject().getId());
+        System.out.println("notifications invoked");
         return true;
     }
 }
