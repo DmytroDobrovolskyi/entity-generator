d7939086f8c5bff24fe2c548c7687055ad348402
DmytroDobrovolskyi <gwini777@gmail.com>
2015-07-29 10:31:47 +0300

    Change NotificationPortImpl.java to controller

--- src/main/java/com/softserve/entity/generator/webservice/impl/NotificationPortImpl.java
+++ src/main/java/com/softserve/entity/generator/webservice/impl/NotificationPortImpl.java
@@ -1,7 +1,11 @@
 package com.softserve.entity.generator.webservice.impl;
 
+import com.softserve.entity.generator.service.EntityService;
 import com.softserve.entity.generator.webservice.NotificationMessageCNotification;
 import com.softserve.entity.generator.webservice.NotificationPort;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -11,10 +15,15 @@ import javax.xml.ws.RequestWrapper;
 import javax.xml.ws.ResponseWrapper;
 import java.util.List;
 
+@Controller
 @WebService(name = "NotificationPort", targetNamespace = "http://soap.sforce.com/2005/09/outbound")
 public class NotificationPortImpl implements NotificationPort
 {
+    private static final Logger logger = Logger.getLogger(NotificationPortImpl.class);
     private static final String TARGET_NAMESPACE = "http://soap.sforce.com/2005/09/outbound";
+
+    @Autowired
+    private EntityService entityService;
 
     @WebMethod
     @WebResult(name = "Ack", targetNamespace = TARGET_NAMESPACE)
@@ -36,8 +45,9 @@ public class NotificationPortImpl implements NotificationPort
     {
         for (NotificationMessageCNotification notificationMessage : notifications)
         {
-            System.out.println(notificationMessage.getSObject().getOperationTypeC().getValue());
-            System.out.println(notificationMessage.getSObject().getSalesforceIdC().getValue());
+            logger.info(notificationMessage.getSObject().getOperationTypeC().getValue());
+            logger.info(notificationMessage.getSObject().getSalesforceIdC().getValue());
+            logger.info(entityService);
         }
         return true;
     }
