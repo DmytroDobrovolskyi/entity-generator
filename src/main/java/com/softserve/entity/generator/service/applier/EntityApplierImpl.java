9dd2e9786661a54b27095ee90d900bc54e60d0e3
DmytroDobrovolskyi <gwini777@gmail.com>
2015-08-07 12:49:04 +0300

    Fix stored-procedure-template.vm

--- src/main/java/com/softserve/entity/generator/service/applier/EntityApplierImpl.java
+++ src/main/java/com/softserve/entity/generator/service/applier/EntityApplierImpl.java
@@ -25,4 +25,23 @@ public class EntityApplierImpl implements EntityApplier
 
         entityManager.createNativeQuery(createProcedureQueryString).executeUpdate();
     }
+
+    @Override
+    @Transactional
+    public void resolveNonExisting()
+    {
+        entityManager.createNativeQuery(
+                "DECLARE @existingTables nvarchar(MAX) " +
+                "SELECT @existingTables = COALESCE(@existingTables + ''',''' ,'') + name " +
+                "FROM sys.tables " +
+                "DECLARE @resolveNonExistingQuery nvarchar(MAX) = " +
+                "( " +
+                    "'UPDATE core_schema.ENTITY " +
+                    "SET Is_Processing_Needed = " +
+                    "WHERE Table_Name " +
+                    "NOT IN (''' +  @existingTables + ''') '" +
+                ") " +
+                "EXEC sp_executesql @resolveNonExistingQuery"
+        ).executeUpdate();
+    }
 }
